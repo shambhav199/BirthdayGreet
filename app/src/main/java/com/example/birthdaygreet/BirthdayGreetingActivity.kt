b7f41f14143cc427709d2e7456eb6b9e372a704d
shambhav199 <btech60009.21@bitmesra.ac.in>
2023-05-19 14:39:00 +0530

    message

---
+++ app/src/main/java/com/example/birthdaygreet/BirthdayGreetingActivity.kt
@@ -0,0 +1,24 @@
+package com.example.birthdaygreet
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+
+    companion object {
+        const val NAME_EXTRA = "name_extra"
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+
+        val name = intent.getStringExtra(NAME_EXTRA)
+        val birthdayGreeting = findViewById<TextView>(R.id.birthdayGreeting)
+        birthdayGreeting.text = "Happy Birthday $name"
+    }
+}
